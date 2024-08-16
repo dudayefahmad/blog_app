c4e91f46534da66180b88f8cf82882f4b2d3783e
Ahmad Dudayef <ahmaddudayef@gmail.com>
2024-08-16 09:22:40 +0700

    Blog Page, Add New Blog Page

--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -26,6 +26,7 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
         _currentUser = currentUser,
         _appUserCubit = appUserCubit,
         super(AuthInitial()) {
+    on<AuthEvent>((_, emit) => emit(AuthLoading()));
     on<AuthSignUp>(_onAuthSignUp);
     on<AuthLogin>(_onAuthLogin);
     on<AuthIsUserLoggedIn>(_isUserLoggedIn);
@@ -47,7 +48,6 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
     AuthSignUp event,
     Emitter<AuthState> emit,
   ) async {
-    emit(AuthLoading());
     final response = await _userSignUp(
       UserSingUpParams(
         email: event.email,
@@ -66,7 +66,6 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
     AuthLogin event,
     Emitter<AuthState> emit,
   ) async {
-    emit(AuthLoading());
     final response = await _userLogin(
       UserLoginParams(
         email: event.email,
